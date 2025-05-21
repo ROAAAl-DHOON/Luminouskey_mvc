6c305d69083518feb40793c4f77632711b45fd39
ROAA AL-DHOON <116735989+ROAAAl-DHOON@users.noreply.github.com>
2025-05-21 13:34:19 +0300

    Task1_mvc

---
+++ Task1_mvc/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Task1_mvc.Models
+{
+    public class Student
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Course { get; set; }
+        public int Age {  get; set; }
+    }
+}
