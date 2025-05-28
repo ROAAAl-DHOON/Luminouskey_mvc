6b321006d7d8137752dacdcc43cd83e255f17135
ROAA AL-DHOON <116735989+ROAAAl-DHOON@users.noreply.github.com>
2025-05-28 10:15:13 +0300

    Task1&2

---
+++ Task2/Controllers/InformationController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task2.Models;
+
+namespace Task2.Controllers
+{
+    public class InformationController : Controller
+    {
+        // GET: Information
+        public ActionResult Index(int? id )
+        {
+           /* Information info1 = new Information();
+            info1.Name = "Roa'a Al-Dhoon";
+            info1.phone_number = "0782525113";
+            info1.Email = "roaaaldhoun111@gmail.com";
+            info1.Summary = "Passionate graduate student at Jordan University of Science and Technology (JUST), with strong programming skills in C#, C++ and Python. Have an excellent experience in web development programming using HTML, CSS, OOP, Verilog, Java Script and SQL.  ";
+            info1.Education = "Jordan University of Science and Technology (JUST) | Computer Engineering \r\n•\tCumulative GPA: 3.35/4.2 (very good high)   \r\n•\tExpected Graduation: 2025 (JUN) \r\n";
+            info1.Skills = "Technical Skills:\r\n-Programming Languages: C#, C++, Python, Verilog, HTML and java Script\r\n-Software Concepts: Object-Oriented Programming (OOP), Data Structures, Algorithms\r\n-Tools: Multi-threading, UML modeling\r\n-Image Processing Techniques\r\n-Database Management: SQL, PL/SQL, ORACLE\r\n";
+            info1.Projects = "•\tShopping Website for books using C# language (using Asp.net).\r\n•\tShopping Website for clothes using C# language (using Asp.net).\r\n•\tGraduation Project: Application that connects to Esp programmed using Flutter language the app will send/receive data to/from the Esp using Firebase (cloud firestore) the app used for blind person in order to help him know if there is an obstacle in front of him and some other features like call emergency, send current location and know the statues of sensors.\r\n";
+            info1.Languages = "-English: Fluent\r\n-Arabic: Native\r\n";
+          
+            return View(info1);*/
+           Information info = new Information();
+            var student = info.GetInfo(id);
+            return View(student);
+        }
+
+    }
+}
---
+++ Task2/Models/Information.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+
+namespace Task2.Models
+{
+    public class Information
+    {
+        public string Name { get; set; }
+        public string Summary { get; set; }
+        public string Email { get; set; }
+        public string phone_number { get; set; }
+        public string Education { get; set; }
+        public string Skills { get; set; }
+        public string Projects { get; set; }
+        public string Languages { get; set; }
+    
+    public Information GetInfo(int? id )
+    {
+        Information info1= new Information();
+        switch( id ) {
+            case 1:
+                {
+                    info1.Name = "Roaa";
+                    info1.Summary = "1111";
+                    info1.Email = "roaa@gmail.com";
+                    info1.phone_number = "0771111111";
+                    info1.Education = "Computer Engineering";
+                    info1.Skills = "1111";
+                    info1.Projects = "1111";
+                    info1.Languages = "English ";
+                    break;
+                }
+            case 2:
+                {
+                    info1.Name = "Shaima";
+                    info1.Summary = "2222";
+                    info1.Email = "shaima@gmail.com";
+                    info1.phone_number = "0771111111";
+                    info1.Education = "Software Engineering";
+                    info1.Skills = "2222";
+                    info1.Projects = "2222";
+                    info1.Languages = "English ";
+                    break;
+                }
+            case 3:
+                {
+                    info1.Name = "Hamzeh";
+                    info1.Summary = "3333";
+                    info1.Email = "hamzeh@gmail.com";
+                    info1.phone_number = "0771111111";
+                    info1.Education = "CS";
+                    info1.Skills = "3333";
+                    info1.Projects = "3333";
+                    info1.Languages = "English ";
+                    break;
+                }
+
+              
+        }
+        return info1;
+    }
+}
+}
